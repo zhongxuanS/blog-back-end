3b6df69533e2ff88bbbbd0f0d34c6b4bdce8452b
John Doe <jdoe@email.com>
2019-08-28 12:48:37 +0800

    add log format

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const logger = require('../lib/common/log4j');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
-router.get('/getCategory', function (req, res) {
-  res.json({ 'isSuccess': true });
+router.post('/getCategory', function (req, res) {
+  logger.debug('test');
+  res.end();
 });
 
 
