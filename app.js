6e2125a9344966c07ae897fa513f7b9217427e3c
John Doe <jdoe@email.com>
2019-09-10 09:52:18 +0800

    fix bug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
